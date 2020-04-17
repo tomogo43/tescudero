68cd37de0199bf808e0000c2704f5e3837e6ef87
tomogo43 <thomas43hteloire@orange.fr>
2020-04-17 15:40:38 +0200

    17/04/20 Initialisation du projet, bootstrap 4.4 & Angular material, tous les components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CurriculumVitaeComponent } from './curriculum-vitae/curriculum-vitae.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { PostsComponent } from './posts/posts.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CurriculumVitaeComponent,
+    ProjetsComponent,
+    FourOhFourComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
