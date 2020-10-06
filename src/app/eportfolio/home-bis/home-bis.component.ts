3223de336e93a050b3b47db83038e2389736f7f1
Thomas ESCUDERO <thomas43hteloire@orange.fr>
2020-10-06 12:06:06 +0200

    améliorations home eportfolio

--- src/app/eportfolio/home-bis/home-bis.component.ts
+++ src/app/eportfolio/home-bis/home-bis.component.ts
@@ -7,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeBisComponent implements OnInit {
 
+  // chargement de la photo de profil
+  loading: boolean = true;
+  
   constructor() { }
 
+  // vérifie si la photo de profil est chargée
+  onLoad() {
+    this.loading = false;
+  }
+  
   ngOnInit(): void {
   }
 
