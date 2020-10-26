4a091931f12ad1562271fbbf166473fd078d1051
Thomas ESCUDERO <thomas43hteloire@orange.fr>
2020-10-26 22:36:32 +0100

    Ajout de la vidéo de présentation dans home et modification de education et du sidenav

--- src/app/eportfolio/home-bis/home-bis.component.ts
+++ src/app/eportfolio/home-bis/home-bis.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-bis',
@@ -10,13 +11,39 @@ export class HomeBisComponent implements OnInit {
   // chargement de la photo de profil
   loading: boolean = true;
   
-  constructor() { }
+  constructor(private router: Router) { }
 
   // vérifie si la photo de profil est chargée
   onLoad() {
     this.loading = false;
   }
   
+  // affiche le CV
+  viewCV() {
+    window.open("http://tescudero.fr/documents/cv_escudero.pdf");
+  }
+
+  // change de component 
+  changeComponent(section) {
+    switch (section) {
+      case "education":
+          this.router.navigate(['/eportfolio/education_R&T']);
+        break;
+      case "apprenticeship":
+          this.router.navigate(['/eportfolio/apprenticeship']);
+          break;
+      case "english":
+          this.router.navigate(['/eportfolio/professional-english']);
+          break;
+      case "activities":
+          this.router.navigate(['/eportfolio/sports']);
+          break;
+      default:
+          this.router.navigate(['/eportfolio/home'])
+        break;
+    }
+  }
+
   ngOnInit(): void {
   }
 
